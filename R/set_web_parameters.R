6786bd20674bdb15e32e20d1e0ff9d4e0f2720ac
CJ Yetman <cyetman@rmi.org>
2023-09-14 10:24:04 +0200

    remove `project_name` parameter
    `project_name` is set to be removed from the TM workflow in https://github.com/RMI-PACTA/workflow.transition.monitor/pull/207 and is a [seemingly unused, leftover parameter](https://github.com/RMI-PACTA/workflow.transition.monitor/pull/207#issue-1892998497)

--- R/set_web_parameters.R
+++ R/set_web_parameters.R
@@ -6,7 +6,6 @@ set_web_parameters <- function(file_path) {
   .GlobalEnv$template_path <- cfg$paths$template_location
   .GlobalEnv$user_results_path <- cfg$paths$user_data_location
 
-  .GlobalEnv$project_name <- cfg$parameters$project_name
   .GlobalEnv$new_data <- cfg$parameters$new_data
 
   .GlobalEnv$financial_timestamp <- cfg$parameters$financial_timestamp
