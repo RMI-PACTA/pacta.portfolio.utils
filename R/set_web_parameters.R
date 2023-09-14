d1f7a08dd61929ca80264741f411bc6b0896d4f8
CJ Yetman <cyetman@rmi.org>
2023-09-14 14:31:26 +0200

    do not look for `financial_timestamp` in "web parameters" (#10)

--- R/set_web_parameters.R
+++ R/set_web_parameters.R
@@ -5,6 +5,4 @@ set_web_parameters <- function(file_path) {
   .GlobalEnv$data_location_ext <- cfg$paths$data_location_ext
   .GlobalEnv$template_path <- cfg$paths$template_location
   .GlobalEnv$user_results_path <- cfg$paths$user_data_location
-
-  .GlobalEnv$financial_timestamp <- cfg$parameters$financial_timestamp
 }
