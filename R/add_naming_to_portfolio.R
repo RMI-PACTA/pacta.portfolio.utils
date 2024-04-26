f1558e2dbc931764df7fc2706e0464c6284fc35e
CJ Yetman - RMI <cjrmi@CJs-MacBook-Pro.fritz.box>
2024-04-26 23:53:41 +0200

    remove unused function `add_naming_to_portfolio()`

--- R/add_naming_to_portfolio.R
+++
@@ -1,6 +0,0 @@
-add_naming_to_portfolio <- function(portfolio_raw) {
-  portfolio_raw$portfolio_name <- .GlobalEnv$portfolio_name
-  portfolio_raw$investor_name <- .GlobalEnv$investor_name
-
-  return(portfolio_raw)
-}
